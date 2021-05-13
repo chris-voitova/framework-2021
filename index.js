b42e385070a07717a69c756b693d9ac46251136c
Chris Voitova <voitova@teamharbour.com>
2021-05-13 11:59:55 +0300

    added server logic

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
 // import { CELSIUS_UNITS } from './utils';
 
+const URL = 'https://api.adviceslip.com/advice';
+
 window.dataStore = {
   advice: '',
+  isLoading: '',
 };
-
-// function SearchByCity() {
-// 	const weatherData = weatherByCity[window.dataStore.currentCity];
-
-// 	return `
-// 	  <input
-// 			type="text"
-// 			value="${window.dataStore.currentCity}"
-// 			onchange="window.dataStore.currentCity = this.value; window.renderApp();"
-// 	  />
-// 	  ${!weatherData ? `Enter one of the city names: ${Object.keys(weatherByCity).join(', ')}.` : ''}
-//  `;
-//  }
-
-const URL = 'https://api.adviceslip.com/advice';
 
 async function getAdvice() {
   try {
@@ -25,13 +13,17 @@ async function getAdvice() {
     if (response.ok) {
       const json = await response.json();
       const advice = json.slip.advice;
-      window.dataStore.advice = advice;
-      // renderAdvice(advice);
-      // console.log(window.dataStore.advice);
-      return renderAdvice(window.dataStore.advice);
+      if (window.dataStore.advice !== advice) {
+        window.dataStore.advice = advice;
+      } else {
+        getAdvice();
+        //   setTimeout(getAdvice, 500);
+      }
+      renderApp();
     }
   } catch {
-    renderAdvice('oops :( Advice fetch failed');
+    window.dataStore.advice = 'oops :( the universe has no advice';
+    renderApp();
     throw Error(response.statusText);
   }
 }
@@ -40,29 +32,30 @@ function renderAdvice(message) {
   return `<div class="advice">${message}</div>`;
 }
 
-function notAsync() {
-  return 'not async data';
-}
-
-async function renderApp() {
+function renderApp() {
   document.getElementById('app-root').innerHTML = `
-        ${await App()}
+        ${App()}
     `;
 }
 
-async function App() {
+function App() {
   return `
   <div>
-    ${Button()}
+    ${getRandomAdvice()}
   </div>`;
 }
 
 window.renderApp = renderApp;
-window.renderAdvice = renderAdvice;
+window.getAdvice = getAdvice;
 
-const Button = () => `
-  <button onclick="window.renderAdvice()">Click me!</button>
-`;
-Button();
+function getRandomAdvice() {
+  return `
+   <button onclick="window.getAdvice()">Universe give me advice!</button>
+	<br>
+	<br>
+	<div>${window.dataStore.isLoading}</div>
+	${renderAdvice(window.dataStore.advice)}
+  `;
+}
 
 renderApp();
